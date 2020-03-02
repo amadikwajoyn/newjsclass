e242a78528658a4f079a68042668ea0e2fa0c8f5
treasurechristain <amadikwajoyn@gmail.com>
2020-03-02 14:51:15 +0100

    edit classsting method

---
+++ classStringMethod.js
@@ -0,0 +1,24 @@
+// some string methods we have
+
+let email = "amadikwajoyn@gmail.com";
+// for lastIndexOf
+ let result1 = email.lastIndexOf('k');
+ console.log(result1);
+
+ // for slice
+ let result2 = email.slice(4,17);
+ console.log(result2);
+
+ // for substr
+ let result3 = email.substr(4, 17);
+ console.log(result3);
+
+//  for replace
+
+let result4 = email.replace('m','w');
+console.log(result4);
+
+
+// for another replace method
+let result5 = email.slice('1, 7');
+console.log(result5);
