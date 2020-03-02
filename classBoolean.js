ed0982312a663f780d0a05647bc82d6856323ae0
treasurechristain <amadikwajoyn@gmail.com>
2020-03-02 17:07:43 +0100

    classBoolean.js and classNullAndUndefine.js

---
+++ classBoolean.js
@@ -0,0 +1,23 @@
+//boolean & comparison
+console.log(false,true,"true", "false");
+
+
+//methods can return boolean
+email = "amadikwajoyn@gmail.com";
+result = email.includes('@');
+console.log(result);
+
+
+// comparison operator
+
+let num = 390;
+
+console.log(num==3);
+console.log(num==390);
+console.log(num!=390);
+console.log(num>3);
+console.log(num<3);
+console.log(num<=3);
+console.log(num>=3);
+console.log(num!==390);
+console.log(email> 'Xam');
---
+++ classNullAndUndefine.js
@@ -0,0 +1,10 @@
+// for undefine
+
+let hage;
+
+console.log(hage, hage+ 2, `the hage is ${hage}`);
+
+
+// for null
+hage = null;
+console.log(hage, hage+ 2, `the hage is ${hage}`);
