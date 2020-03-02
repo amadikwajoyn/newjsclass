ff3afd5dfbb8c17e93fbb04be46917355c36ccec
treasurechristain <amadikwajoyn@gmail.com>
2020-03-02 14:18:51 +0100

    first commit

---
+++ classIntro.js
@@ -0,0 +1,36 @@
+console.log(1);
+console.log(2);
+
+let age = 24;
+let year = 2991;
+
+//working with string
+let name = "Joy";
+console.log(name);
+
+// sting concantenation
+let name2 = "Amadikwa"
+
+console.log(name2 +' ' + name)
+// log thing to console
+console.log(age, year);
+
+age = 40;
+console.log(age);
+const  pi = 3.14;
+console.log(pi);
+
+//using the var keyword
+var score = 73;
+console.log(score);
+
+// getting characters
+console.log(name2[0]);
+
+
+// string length
+console.log(name2.length);
+
+// string method
+console.log(name2.toUpperCase());
+console.log(name2.toLocaleUpperCase());
