01b0ac09e6c0dc4be7918c07867b58e41479807d
treasurechristain <amadikwajoyn@gmail.com>
2020-03-02 17:21:14 +0100

    add classTypeConversion.js

---
+++ classTypeConversion.js
@@ -0,0 +1,21 @@
+score = "1002";
+console.log(score + 1);
+score = Number(score);
+
+console.log(score + 1);
+
+
+// for string 
+result = String(50);
+ console.log(result, typeof result);
+
+ // for boolean
+
+ result = Boolean(0);
+ console.log(result);
+ result = Boolean('0');
+ console.log(result);
+ result = Boolean('');
+ console.log(result);
+ result = Boolean(1);
+ console.log(result);
