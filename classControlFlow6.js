824c5b676ded62cefbe5befb2658ec1253f52cb2
treasurechristain <amadikwajoyn@gmail.com>
2020-03-05 14:47:32 +0100

    add switch statement

---
+++ classControlFlow6.js
@@ -0,0 +1,22 @@
+// switch statement
+ const grades = "E";
+
+switch (grades) {
+    case 'A':
+        console.log('You got an A!');
+        break;
+    case 'B':
+        console.log('You got a B!');
+        break;
+    case 'C':
+        console.log('You got a C!');break;
+    case 'D':
+        console.log('You got a D!');
+        break;
+    case 'E':
+        console.log('You got a E!');
+        break;
+    default:
+        console.log('Not a valid grade')
+        break;
+}
