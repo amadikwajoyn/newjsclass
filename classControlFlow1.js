106992ddf0bf18d3edf50b67aaf24c58eee06e67
treasurechristain <amadikwajoyn@gmail.com>
2020-03-05 11:30:56 +0100

    added classControlFlow1.js

---
+++ classControlFlow1.js
@@ -0,0 +1,7 @@
+//for loops
+let names = ['Joy', 'Cynthia', 'Codebug'];
+for(let i = 0; i < names.length; i++){
+    let html = `<div>The name is ${names[i]}</div>`
+    console.log(html);
+}
+console.log('finished');
