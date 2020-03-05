850cf9a2282b4cbf17cc289fc032e500f075b32a
treasurechristain <amadikwajoyn@gmail.com>
2020-03-05 12:55:50 +0100

    add while loop and do while loop

---
+++ classControlFlow2.js
@@ -0,0 +1,8 @@
+// while loop
+
+let names = ['codebug', 'Joy', 'Treasure', 'Nmesomachi'];
+let i = 0;
+while (i < 6) {
+    console.log('Normsoh am learning Javascript now, is very fun', i);
+    i++;
+}
---
+++ classControlFlow3.js
@@ -0,0 +1,6 @@
+let i = 0;
+
+do{
+    console.log('the number is ', i);
+    i++;
+} while(i < 5);
