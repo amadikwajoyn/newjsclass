c45f078a102de4db0a4a0c24f8fde8db9b6a7e78
treasurechristain <amadikwajoyn@gmail.com>
2020-03-06 11:52:06 +0100

    edit classFunction.js and index.html file

--- classFunction.js
+++ classFunction.js
@@ -1,3 +1,22 @@
+
+const bill1 = function(products1, tax1){
+    let total1 = 0;
+    for (let i = 0; i < products1.length; i++) {
+        total1 += products1[i] + products1[i] * tax1;
+    };
+     
+    return total1;
+};
+
+console.log(bill1([23,34,45], 0.4));
+
+
+//arrow functions
+const greeting = ()=> 'good morning guys';
+
+console.log(greeting());
+
+
 const greet = () => 'hello, world';
 
 console.log(greet());
@@ -11,3 +30,27 @@ const bill = (products, tax ) => {
 };
 
 console.log(bill([203,43,56],0.5));
+
+
+// for call back function 
+
+const myFunc = callBackFunct => {
+    let number = 23;
+    callBackFunct(number);
+};
+
+myFunc(number=> console.log(number));
+
+
+// using innerhtml function
+const ul = document.querySelector('.people');
+const students = ['Joy', 'Amadikwa', 'Nmesomachi', 'Treasure', 'Cynthia', 'Ugo', 'Normsoh', 'Chigaze'];
+
+//declare the html format element
+let htmls =  ``;
+students.forEach(student=>{
+    htmls += `<li style ="color: red; font-size:20px;">${student}</li>` ;
+});
+
+ul.innerHTML = htmls;
+
